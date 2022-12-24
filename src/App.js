94ffc9229fa0c07bad6f390f306d80cc0a19987a
rogervalentim <valentim.roger33@gmail.com>
2022-12-24 11:27:19 -0300

    add Breakdown page

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Geography from 'scenes/geography';
 import Overview from 'scenes/overview';
 import Daily from 'scenes/daily';
 import Monthly from 'scenes/monthly';
+import Breakdown from 'scenes/breakdown';
 
 function App() {
     const mode = useSelector((state) => state.global.mode);
@@ -33,6 +34,7 @@ function App() {
                             <Route path="/overview" element={<Overview />}></Route>
                             <Route path="/daily" element={<Daily />}></Route>
                             <Route path="/monthly" element={<Monthly />}></Route>
+                            <Route path="/breakdown" element={<Breakdown />}></Route>
 
                         </Route>
                     </Routes>
