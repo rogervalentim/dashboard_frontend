01532d93c5a20e3ca5e8b48c73fb084beb863955
rogervalentim <valentim.roger33@gmail.com>
2022-12-25 16:09:15 -0300

    add admins page

--- src/scenes/admin/index.jsx
+++ src/scenes/admin/index.jsx
@@ -1,28 +1,29 @@
 import React from "react";
 import { Box, useTheme } from "@mui/material";
-import { useGetCustomersQuery } from "state/api";
-import Header from "components/Header";
+import { useGetAdminsQuery } from "state/api";
 import { DataGrid } from "@mui/x-data-grid";
+import Header from "components/Header";
+import CustomColumnMenu from "components/DataGridCustomColumnMenu";
 
-const Customers = () => {
+const Admin = () => {
   const theme = useTheme();
-  const { data, isLoading } = useGetCustomersQuery();
+  const { data, isLoading } = useGetAdminsQuery();
 
   const columns = [
     {
       field: "_id",
       headerName: "ID",
-      flex: 1,
+      flex: 1
     },
     {
       field: "name",
       headerName: "Name",
-      flex: 0.5,
+      flex: 0.5
     },
     {
       field: "email",
       headerName: "Email",
-      flex: 1,
+      flex: 1
     },
     {
       field: "phoneNumber",
@@ -30,54 +31,54 @@ const Customers = () => {
       flex: 0.5,
       renderCell: (params) => {
         return params.value.replace(/^(\d{3})(\d{3})(\d{4})/, "($1)$2-$3");
-      },
+      }
     },
     {
       field: "country",
       headerName: "Country",
-      flex: 0.4,
+      flex: 0.4
     },
     {
       field: "occupation",
       headerName: "Occupation",
-      flex: 1,
+      flex: 1
     },
     {
       field: "role",
       headerName: "Role",
-      flex: 0.5,
-    },
+      flex: 0.5
+    }
   ];
 
   return (
     <Box m="1.5rem 2.5rem">
-      <Header title="CUSTOMERS" subtitle="List of Customers" />
+      <Header title="ADMINS" subtitle="Managing admins and list of admins" />
       <Box
         mt="40px"
         height="75vh"
         sx={{
           "& .MuiDataGrid-root": {
-            border: "none",
+            border: "none"
           },
           "& .MuiDataGrid-cell": {
-            borderBottom: "none",
+            borderBottom: "none"
           },
           "& .MuiDataGrid-columnHeaders": {
             backgroundColor: theme.palette.background.alt,
             color: theme.palette.secondary[100],
-            borderBottom: "none",
+            borderBottom: "none"
           },
           "& .MuiDataGrid-virtualScroller": {
-            backgroundColor: theme.palette.primary.light,
+            backgroundColor: theme.palette.primary.light
           },
           "& .MuiDataGrid-footerContainer": {
             backgroundColor: theme.palette.background.alt,
             color: theme.palette.secondary[100],
-            borderTop: "none",
+            borderTop: "none"
           },
           "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
-            color: `${theme.palette.secondary[200]} !important`,
-          },
+            color: `${theme.palette.secondary[200]} !important`
+          }
         }}
       >
         <DataGrid
@@ -85,10 +86,13 @@ const Customers = () => {
           getRowId={(row) => row._id}
           rows={data || []}
           columns={columns}
+          components={{
+            ColumnMenu: CustomColumnMenu,
+          }}
         />
       </Box>
     </Box>
   );
 };
 
-export default Customers;
+export default Admin;
